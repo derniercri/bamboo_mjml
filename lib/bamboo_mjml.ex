46814e7a2600fbd5001c43e4f7cf8f4836e9a4c5
julien-leclercq <julien.leclercq@derniercri.io>
2017-08-14 17:46:05 +0200

    first commit

---
+++ lib/bamboo_mjml.ex
@@ -0,0 +1,18 @@
+defmodule BambooMjml do
+  @moduledoc """
+  Documentation for BambooMjml.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BambooMjml.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bamboo_mjml_test.exs
@@ -0,0 +1,8 @@
+defmodule BambooMjmlTest do
+  use ExUnit.Case
+  doctest BambooMjml
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
